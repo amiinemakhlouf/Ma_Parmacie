1c211706ac6e95ab712b72b61d82847193fb5d41
amine makhlouf <amiinemakhlouf@gmail.com>
2023-02-19 08:59:06 +0100

    codabar with zhang work

--- app/src/main/java/esprims/gi2/ma_pharmacie/MainActivity.kt
+++ app/src/main/java/esprims/gi2/ma_pharmacie/MainActivity.kt
@@ -1,22 +1,39 @@
 package esprims.gi2.ma_pharmacie
 
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toBitmap
-import com.google.mlkit.vision.barcode.BarcodeScannerOptions
-import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.barcode.common.Barcode
-import com.google.mlkit.vision.common.InputImage
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+import esprims.gi2.ma_pharmacie.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val image = resources.getDrawable(R.drawable.code2).toBitmap()
 
+        binding.scanButton.setOnClickListener {
+          barcodeLauncher.launch(ScanOptions())
 
-        val image = resources.getDrawable(R.drawable.barcode).toBitmap()
-
+        }
     }
-}
+
+    val barcodeLauncher = registerForActivityResult(ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this@MainActivity,
+                result.contents,
+                Toast.LENGTH_LONG).show()
+            Log.d("my result",result.contents)
+        }
+
+}}
