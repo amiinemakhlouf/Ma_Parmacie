e273bfd1c983b30ddd4ffeb1cd24238d29eebb30
amine makhlouf <amiinemakhlouf@gmail.com>
2023-02-19 10:57:11 +0100

    layering project by clean architecture

---
+++ app/src/main/java/esprims/gi2/ma_pharmacie/presentation/MainActivity.kt
@@ -0,0 +1,15 @@
+package esprims.gi2.ma_pharmacie.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import esprims.gi2.ma_pharmacie.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
