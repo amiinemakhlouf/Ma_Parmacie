b3bed3b8162c500f851f6ee73632df9a29624d60
amine makhlouf <amiinemakhlouf@gmail.com>
2023-02-24 08:45:31 +0100

    add splash screen

--- app/src/main/java/esprims/gi2/ma_pharmacie/MainActivity.kt
+++ app/src/main/java/esprims/gi2/ma_pharmacie/MainActivity.kt
@@ -5,42 +5,19 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toBitmap
-import com.journeyapps.barcodescanner.ScanContract
-import com.journeyapps.barcodescanner.ScanIntentResult
-import com.journeyapps.barcodescanner.ScanOptions
 import esprims.gi2.ma_pharmacie.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private  lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val image = resources.getDrawable(R.drawable.code2).toBitmap()
 
         binding.scanButton.setOnClickListener {
-          barcodeLauncher.launch(ScanOptions())
 
         }
     }
 
-    val barcodeLauncher = registerForActivityResult(ScanContract()
-    ) { result: ScanIntentResult ->
-        if (result.contents == null) {
-            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_LONG).show()
-        } else {
-            if(result.contents=="6192012110632")
-            {
-                Toast.makeText(this,"this is aflomad",Toast.LENGTH_LONG).show()
-            }
-            else if (result.contents=="6192402816519"){
-                Toast.makeText(this@MainActivity,
-                    "opalia",
-                    Toast.LENGTH_LONG).show()
-                Log.d("my result",result.contents)
-
-            }
-        }
-
-}}
+}
