b4b06d35fc7dcb94813304b9b349c16858c9c446
amine makhlouf <amiinemakhlouf@gmail.com>
2023-03-03 08:47:53 +0100

    schedule alarm  manager

--- app/src/main/java/esprims/gi2/ma_pharmacie/presentation/main/MainActivity.kt
+++ app/src/main/java/esprims/gi2/ma_pharmacie/presentation/main/MainActivity.kt
@@ -2,46 +2,32 @@ package esprims.gi2.ma_pharmacie.presentation.main
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Intent
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.text.format.DateUtils
-import android.util.Log
-import android.view.MenuItem
-import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.AlarmManagerCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import esprims.gi2.ma_pharmacie.R
 import esprims.gi2.ma_pharmacie.databinding.ActivityMainBinding
-import esprims.gi2.ma_pharmacie.domain.AlarmReceiver
 import esprims.gi2.ma_pharmacie.presentation.AlarmActivity
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController:NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private  var toast:Toast?=null
 
     private val viewModel: MainActivityViewModel by viewModels()
     @RequiresApi(Build.VERSION_CODES.M)
@@ -49,8 +35,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val intent=Intent(this,AlarmActivity::class.java)
-
         startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION))
+        val t=Toast.makeText(this,"i'm amine",Toast.LENGTH_SHORT).show()
         binding.bt.setOnClickListener {
 
             val alarmManager =getSystemService(AlarmManager::class.java)
@@ -69,6 +55,7 @@ class MainActivity : AppCompatActivity() {
             )
 
         }
+
 
 
         setContentView(binding.root)
@@ -105,5 +92,7 @@ class MainActivity : AppCompatActivity() {
         binding.navigationView.setupWithNavController(navController)
 
     }
+
+
 
 }
